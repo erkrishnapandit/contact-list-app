38741ee3c8c8fb3e56510d2eb9a6c516b42cc6f1
Krishna Kumar Pandit <krishnakumarji100@gmail.com>
2023-08-26 18:19:15 +0530

    Styling Final Tuch Done

--- src/AddContactForm.js
+++ src/AddContactForm.js
@@ -1,14 +1,18 @@
 import React, { useState } from 'react';
 
+// Component To Add New Contact.
 function AddContactForm({ onAddContact }) {
+  // React useState To set Contact Details.
   const [name, setName] = useState('');
   const [phone, setPhone] = useState('');
   const [email, setEmail] = useState('');
 
+  // This handleSubmit Funtionality will Add Contact Details Into The List When Click On Submit Button.
   const handleSubmit = event => {
     event.preventDefault();
     if (name.trim() !== '' && phone.trim() !== '' && email.trim() !== '') {
       onAddContact({ name, phone, email });
+      // Setting Input Detail Field As Blank After Adding Details Into List.
       setName('');
       setPhone('');
       setEmail('');
@@ -17,6 +21,7 @@ function AddContactForm({ onAddContact }) {
 
   return (
     <form onSubmit={handleSubmit}>
+      {/* Taking Contact Name As Input */}
       <input
         type="text"
         placeholder="Name*"
@@ -24,6 +29,7 @@ function AddContactForm({ onAddContact }) {
         onChange={event => setName(event.target.value)}
         required
       />
+      {/* Taking Phone Number As Input */}
       <input
         type="number"
         placeholder="Phone*"
@@ -31,6 +37,7 @@ function AddContactForm({ onAddContact }) {
         onChange={event => setPhone(event.target.value)}
         required
       />
+      {/* Taking Email Id As Input */}
       <input
         type="email"
         placeholder="Email*"
@@ -38,9 +45,11 @@ function AddContactForm({ onAddContact }) {
         onChange={event => setEmail(event.target.value)}
         required
       />
+      {/* Add Contact Button */}
       <button id='add-contact-btn' type="submit">Add Contact</button>
     </form>
   );
 }
 
+// Default Exporting
 export default AddContactForm;
